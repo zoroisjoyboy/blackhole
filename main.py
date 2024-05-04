2d952b3a79a13160bb1462048b1d780d5046b10c
allensaakov <allengasa4@gmail.com>
2024-05-04 07:24:15 -0500

    galaxy implemented

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import grid
 
 WINDOW_WIDTH = 1920
 WINDOW_HEIGHT = 1080
-CELL_SIZE = 3
+CELL_SIZE = 7
 PADDING = 1
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT)) # maybe have a map bigger than what is represented? 
+    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.RESIZABLE) # fill screen, dynamic window implement
     myfont = pygame.freetype.Font(None, CELL_SIZE + PADDING)
     clock = pygame.time.Clock()
     total_cell_size = CELL_SIZE + PADDING
@@ -26,6 +26,8 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_F11:
+                pygame.display.toggle_fullscreen()
 
         keys = pygame.key.get_pressed()
         
@@ -46,7 +48,12 @@ if __name__ == "__main__":
                 y = r * (CELL_SIZE + PADDING) + PADDING     
                 match g.grid[r][c]:
                     case 1:
-                        myfont.render_to(screen, (x, y), g.random_ascii(), (255, 255, 255)) # maybe slow down the tickle        
+                        myfont.render_to(screen, (x, y), chr(115), (255, 236, 236)) # color changes based off red shift or blue shift
+                    case 2:
+                        myfont.render_to(screen, (x, y), chr(103), (255, 236, 236))
+                    case 3:
+                        # screen.fill("black", (x, y, CELL_SIZE, CELL_SIZE))
+                        myfont.render_to(screen, (x, y), "3", (255, 255, 255))        
                                                                                            
         pygame.display.flip()
         clock.tick(60)
